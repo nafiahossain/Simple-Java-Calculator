1753a1d302521fcfdd6713af0a7a87aa4a061031
nafiahossain <nafiahossain12@gmail.com>
2022-05-17 22:30:23 +0600

    Hossain

--- src/javafile.java
+++ src/javafile.java
@@ -1,5 +1,5 @@
 public class Person {
-  private String name; // private = restricted access
+  private String name="Hossain"; // private = restricted access
 
   // Getter
   public String getName() {
